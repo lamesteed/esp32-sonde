102977ee9e8f778a9d1662fd7f53d2ae9bd75fc7
KHARLANAU, VADZIM <vadzim.kharlanau@gmail.com>
2025-01-17 20:02:27 -0500

    Adds doxygen comments

--- main/libs/command/CRebootCommand.h
+++ main/libs/command/CRebootCommand.h
@@ -5,21 +5,33 @@
 #include "IDataPublisherService.h"
 #include "IRebootable.h"
 
+/// @brief Command to reboot the device
 class CRebootCommand : public ICommand
 {
 public:
+    /// @brief Constructor
+    /// @param publisher    data publisher interface
+    /// @param rebootable   rebootable interface
     CRebootCommand(
         const IDataPublisherService::Ptr & publisher,
         const IRebootable::Ptr & rebootable );
+
+    /// @brief Virtual destructor
     virtual ~CRebootCommand() = default;
 
 private:
     // ICommand interface
+
+    /// @brief Get command description
+    /// @return Human firendly description of the command
     virtual std::string getDescription() const override;
+
+    /// @brief Execute the command
+    /// @return execution result, true if command executed successfully, false otherwise
     virtual bool execute() override;
 
-    IDataPublisherService::Ptr mPublisher;
-    IRebootable::Ptr mRebootable;
+    IDataPublisherService::Ptr mPublisher;  ///< Data publisher interface
+    IRebootable::Ptr mRebootable;           ///< Rebootable interface
 };
 
 #endif // CREBOOTCOMMAND_H
--- main/libs/command/CTestModeCommand.h
+++ main/libs/command/CTestModeCommand.h
@@ -5,23 +5,35 @@
 #include "ISampler.h"
 #include "IDataPublisherService.h"
 
+/// @brief Command to perform sonde basic sensors testing
 class CTestModeCommand : public ICommand
 {
 public:
+    /// @brief Constructor
+    /// @param sampler      Sampler instance (to read sensor data)
+    /// @param publisher    Data publisher instance (to report execution result)
+    /// @param samplesCount Number of samples to collect
     CTestModeCommand(
         const ISampler::Ptr & sampler,
         const IDataPublisherService::Ptr & publisher, int samplesCount );
+
+    /// @brief Virtual destructor
     virtual ~CTestModeCommand() = default;
 
 private:
     // ICommand interface
+    /// @brief Get command description
+    /// @return Human firendly description of the command
     virtual std::string getDescription() const override;
+
+    /// @brief Execute the command
+    /// @return execution result, true if command executed successfully, false otherwise
     virtual bool execute() override;
 
 private:
-    ISampler::Ptr mSampler;
-    IDataPublisherService::Ptr mPublisher;
-    int mSamplesCount;
+    ISampler::Ptr mSampler;                 ///< Sampler instance
+    IDataPublisherService::Ptr mPublisher;  ///< Data publisher interface
+    int mSamplesCount;                      ///< Number of samples to collect
 };
 
 #endif // CTESTMODECOMMAND_H
