ed03a626d935696248bab4b503f383f0a3691913
KHARLANAU, VADZIM <vadzim.kharlanau@gmail.com>
2025-02-04 10:53:45 -0500

    Adds annother BT service characteristics that is used to transmit command arguments

--- main/libs/publisher/CBluetoothPublisherService.h
+++ main/libs/publisher/CBluetoothPublisherService.h
@@ -38,22 +38,27 @@ private:
     static const char * TAG;                ///< Logger tag
     static const char * BLE_DEVICE_NAME;    ///< BLE device name
     static const char * SERVICE_UUID;       ///< UUID for service
-    static const char * CHAR_NOTIFY_UUID;   ///< UUID for notification characteristic (used to send data to client)
-    static const char * CHAR_RX_CMD_UUID;   ///< UUID for Read/Write characteristic (client uses it to initiate data transfer)
+    static const char * CHAR_NOTIFY_UUID;   ///< UUID for notification characteristic (is used to send command output to client)
+    static const char * CHAR_RX_CMD_UUID;   ///< UUID for Read/Write command characteristic (client uses it to specify trigger command for execution)
+    static const char * CHAR_RX_ARGS_UUID;  ///< UUID for Read/Write command arguments characteristic (client uses it to specify command arguments)
 
     ICommandListener * mNotificationListener;  ///< Listener for commands from BT client
 
-    BLEServer         * mServer;                ///< BLE server instance
-    BLECharacteristic * mNotifyCharacteristic;  ///< Notify characteristic to send data to client
-    BLECharacteristic * mRxCmdCharacteristic;   ///< Read/Write characteristic to initiate data transfer
+    BLEServer         * mServer;                    ///< BLE server instance
+    BLECharacteristic * mNotifyCharacteristic;      ///< Notify characteristic to send command output to client
+    BLECharacteristic * mRxCmdCharacteristic;       ///< Read/Write characteristic to initiate command execution
+    BLECharacteristic * mRxCmdArgsCharacteristic;   ///< Read/Write characteristic to
 
     std::shared_ptr<BLEDescriptor> mNotifyDescriptor2901; ///< BLE descriptor 2901 for Notify characteristic
     std::shared_ptr<BLE2902> mNotifyDescriptor2902;       ///< BLE descriptor 2902 for Notify characteristic
-    std::shared_ptr<BLEDescriptor> mRxDescriptor2901;     ///< BLE descriptor 2901 for Read/Write characteristic
-    std::shared_ptr<BLE2902> mRxDescriptor2902;           ///< BLE descriptor 2902 for Read/Write characteristic
+    std::shared_ptr<BLEDescriptor> mRxDescriptor2901;     ///< BLE descriptor 2901 for Read/Write command characteristic
+    std::shared_ptr<BLE2902> mRxDescriptor2902;           ///< BLE descriptor 2902 for Read/Write command characteristic
+    std::shared_ptr<BLEDescriptor> mRxArgsDescriptor2901; ///< BLE descriptor 2901 for Read/Write command arguments characteristic
+    std::shared_ptr<BLE2902> mRxArgsDescriptor2902;       ///< BLE descriptor 2902 for Read/Write command arguments characteristic
 
     bool mDeviceConnected;                  ///< Flag indicating if device is connected
     std::string mCurrentCommand;            ///< Current command received from client
+    std::string mCurrentCommandArgs;        ///< Current command arguments received from client
 };
 
 #endif // CBLUETOOTHPUBLISHERSERVICE_H
