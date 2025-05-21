da2466f5b47c665ec2302df0984e240792adb186
KHARLANAU, VADZIM <vadzim.kharlanau@gmail.com>
2025-05-21 16:09:58 -0400

    Updates dummy sampler to generate 120 samples

--- main/libs/sampler/CDummySampler.cpp
+++ main/libs/sampler/CDummySampler.cpp
@@ -2,14 +2,13 @@
 
 CDummySampler::CDummySampler( const ITimeService::Ptr & timeService )
     : mTimeService( timeService )
-    , mSamples(10)
+    , mSamples(120)
     , mSampleIndex( 0 )
 {
     // Initialize the sample data with dummy values
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 60; ++i) {
         mSamples[i] = std::make_shared<SampleData>();
         mSamples[i]->temperature = 25.0f + i; // Dummy temperature
-        mSamples[i]->pressure = 1013.25f + i; // Dummy pressure
         mSamples[i]->tds = 500.0f + i;        // Dummy TDS
         mSamples[i]->conductivity = 1000.0f + i; // Dummy conductivity
         mSamples[i]->ph = 7.0f + i;          // Dummy pH
@@ -17,21 +16,26 @@ CDummySampler::CDummySampler( const ITimeService::Ptr & timeService )
         mSamples[i]->depth = 0.0f + i;       // Dummy depth
         mSamples[i]->tsStart = 0;            // Dummy start timestamp
         mSamples[i]->tsEnd = 0;              // Dummy end timestamp
+        mSamples[i]->depth = -1 * (0.98 * (i + 1) ); // Dummy depth
     }
 
-    mSamples[0]->depth = -0.9;
-    mSamples[1]->depth = -2.05;
-    mSamples[2]->depth = -2.98;
-    mSamples[3]->depth = -4.1;
-    mSamples[4]->depth = -5.05; // bottom
-    mSamples[5]->depth = -3.99;
-    mSamples[6]->depth = -3.07;
-    mSamples[7]->depth = -1.92;
-    mSamples[8]->depth = -1.1;
-    mSamples[9]->depth = 0;
+    for (int i = 60; i < 120; ++i) {
+        mSamples[i] = std::make_shared<SampleData>();
+        mSamples[i]->temperature = 25.0f + i; // Dummy temperature
+        mSamples[i]->tds = 500.0f + i;        // Dummy TDS
+        mSamples[i]->conductivity = 1000.0f + i; // Dummy conductivity
+        mSamples[i]->ph = 7.0f + i;          // Dummy pH
+        mSamples[i]->do2 = 8.0f + i;         // Dummy DO2
+        mSamples[i]->depth = 0.0f + i;       // Dummy depth
+        mSamples[i]->tsStart = 0;            // Dummy start timestamp
+        mSamples[i]->tsEnd = 0;              // Dummy end timestamp
+        mSamples[i]->depth = -1 * (120 - 0.99 * i);  // Dummy depth
+    }
+
+    mSamples[119]->depth = 0.0f; // Dummy depth for the last sample
 
     // calculate pressure using formula:  pressure, psi  = depth, m / -0.703
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 120; ++i) {
         mSamples[i]->pressure = mSamples[i]->depth / -0.703;
     }
 
